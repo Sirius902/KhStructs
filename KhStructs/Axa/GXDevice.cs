ea9f6ada06379c57316b91913150911cb5d875b1
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-12-02 12:19:48 -0800

    Fix GXDevice.InitSwapchain incorrect return type and use TerraFX types

--- KhStructs/Axa/GXDevice.cs
+++ KhStructs/Axa/GXDevice.cs
@@ -1,3 +1,6 @@
+using TerraFX.Interop.DirectX;
+using TerraFX.Interop.Windows;
+
 namespace KhStructs.Axa;
 
 // Axa::GX::GXDevice::GXDeviceImpl
@@ -12,68 +15,32 @@ namespace KhStructs.Axa;
 public unsafe partial struct GXDevice {
     [FieldOffset(0x00)] public void* vtbl;
 
-    /// <summary>
-    /// ID3D12Device*
-    /// </summary>
-    [FieldOffset(0x2E0)] public void* D3D12Device;
+    [FieldOffset(0x2E0)] public ID3D12Device* Device;
 
     [FieldOffset(0x32C)] public uint VSyncEnabled;
 
     [FieldOffset(0x3E8)] public AppInterface* App;
-    [FieldOffset(0x3F0)] public nint AxaSimpleWindowHwnd;
-    [FieldOffset(0x3F8)] public nint Hwnd3F8;
+    [FieldOffset(0x3F0)] public HWND AxaSimpleWindowHwnd;
+    [FieldOffset(0x3F8)] public HWND Hwnd3F8;
 
-    /// <summary>
-    /// IDXGISwapChain3*
-    /// </summary>
-    [FieldOffset(0x468)] public void* DXGISwapChain;
-    /// <summary>
-    /// ID3D12CommandQueue*
-    /// </summary>
-    [FieldOffset(0x470)] public void* D3D12MainCommandQueue;
-    /// <summary>
-    /// ID3D12RootSignature*
-    /// </summary>
-    [FieldOffset(0x478)] public void* D3D12RootSignature;
+    [FieldOffset(0x468)] public IDXGISwapChain3* SwapChain;
+    [FieldOffset(0x470)] public ID3D12CommandQueue* MainCommandQueue;
+    [FieldOffset(0x478)] public ID3D12RootSignature* RootSignature;
 
-    /// <summary>
-    /// ID3D12CommandQueue*
-    /// </summary>
-    [FieldOffset(0x488)] public void* D3D12CommandQueue488;
+    [FieldOffset(0x488)] public ID3D12CommandQueue* CommandQueue488;
 
-    /// <summary>
-    /// ID3D12GraphicsCommandList*
-    /// </summary>
-    [FieldOffset(0x498)] public void* D3D12GraphicsCommandList;
-    /// <summary>
-    /// ID3D12CommandAllocator*
-    /// </summary>
-    [FieldOffset(0x4A0)] public void* D3D12CommandAllocator;
-    /// <summary>
-    /// IDXGIOutput*
-    /// </summary>
-    [FieldOffset(0x4A8)] public void* DXGIOutput;
-    /// <summary>
-    /// IDXGIAdapter3*
-    /// </summary>
-    [FieldOffset(0x4B0)] public void* DXGIAdapter;
-    /// <summary>
-    /// ID3D12DescriptorHeap*
-    /// </summary>
-    [FieldOffset(0x4B8)] public void* D3D12RtvDescriptorHeap;
-    /// <summary>
-    /// ID3D12DescriptorHeap*
-    /// </summary>
-    [FieldOffset(0x4C0)] public void* D3D12DsvDescriptorHeap;
+    [FieldOffset(0x498)] public ID3D12GraphicsCommandList* GraphicsCommandList;
+    [FieldOffset(0x4A0)] public ID3D12CommandAllocator* CommandAllocator;
+    [FieldOffset(0x4A8)] public IDXGIOutput* Output;
+    [FieldOffset(0x4B0)] public IDXGIAdapter3* Adapter;
+    [FieldOffset(0x4B8)] public ID3D12DescriptorHeap* RtvDescriptorHeap;
+    [FieldOffset(0x4C0)] public ID3D12DescriptorHeap* DsvDescriptorHeap;
     [FieldOffset(0x4C8)] public void* PStruct4C8;
-    /// <summary>
-    /// ID3D12DescriptorHeap*
-    /// </summary>
-    [FieldOffset(0x4D0)] public void* D3D12SamplerDescriptorHeap;
+    [FieldOffset(0x4D0)] public ID3D12DescriptorHeap* SamplerDescriptorHeap;
 
     [StaticAddress("75 99 48 8D 0D ?? ?? ?? ??", 5, isPointer: false)]
     public static partial GXDevice* Instance();
 
     [MemberFunction("E8 ?? ?? ?? ?? 89 AF ?? ?? ?? ?? F3 0F 10 47 ??")]
-    public static partial void InitSwapchain(GXDevice* device);
+    public static partial HRESULT InitSwapchain(GXDevice* device);
 }
