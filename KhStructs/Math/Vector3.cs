3947939ffac1b0caebafb58eeacd7af58dff7543
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-12-04 11:57:35 -0800

    Implicit operators for vectors

--- KhStructs/Math/Vector3.cs
+++ KhStructs/Math/Vector3.cs
@@ -5,4 +5,13 @@ public struct Vector3 {
     public float X;
     public float Y;
     public float Z;
+
+    public Vector3(float x, float y, float z) {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+    }
+
+    public static implicit operator System.Numerics.Vector3(Vector3 vec) => new(vec.X, vec.Y, vec.Z);
+    public static implicit operator Vector3(System.Numerics.Vector3 vec) => new(vec.X, vec.Y, vec.Z);
 }
--- KhStructs/Math/Vector4.cs
+++ KhStructs/Math/Vector4.cs
@@ -6,4 +6,14 @@ public struct Vector4 {
     public float Y;
     public float Z;
     public float W;
+
+    public Vector4(float x, float y, float z, float w) {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+        this.W = w;
+    }
+
+    public static implicit operator System.Numerics.Vector4(Vector4 vec) => new(vec.X, vec.Y, vec.Z, vec.W);
+    public static implicit operator Vector4(System.Numerics.Vector4 vec) => new(vec.X, vec.Y, vec.Z, vec.W);
 }
