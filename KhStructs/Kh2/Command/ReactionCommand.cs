4c8a153461ccb0806c5dc569ebb0b85082cecd51
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2024-10-13 19:14:41 -0700

    feat: Add target object to ReactionCommand

--- KhStructs/Kh2/Command/ReactionCommand.cs
+++ KhStructs/Kh2/Command/ReactionCommand.cs
@@ -1,3 +1,5 @@
+using KhStructs.Kh2.Object;
+
 namespace KhStructs.Kh2.Command;
 
 // size=???
@@ -5,7 +7,11 @@ namespace KhStructs.Kh2.Command;
 public unsafe partial struct ReactionCommand {
     [FieldOffset(0x00)] public PlayerCommand* PlayerCommand;
 
+    [FieldOffset(0x18)] public Hash<GameObject> TargetObjectHash;
+
     [FieldOffset(0x30)] public CommandMenuEntry Command;
+
+    public GameObject* TargetObject => this.TargetObjectHash.Lookup();
 
     [MemberFunction("E8 ?? ?? ?? ?? 8B 47 24 C1 E8 06")]
     public partial void Update();
